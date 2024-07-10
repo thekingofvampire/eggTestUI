58e7f3e9ffe7b137025a8be4769628645522384c
egg <egg.huang@withinspace.com>
2024-07-10 10:02:58 +0800

    fix(stylelint): change stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   // 继承的预设，这些预设包含了规则集插件
   extends: [
-    // 代码风格规则
-    'stylelint-stylistic/config',
     // 基本 scss 规则
     'stylelint-config-standard-scss',
     // scss vue 规则
@@ -10,6 +8,7 @@ module.exports = {
     // 样式属性顺序规则
     'stylelint-config-recess-order',
   ],
+  plugins: ['@stylistic/stylelint-plugin'],
   rules: {
     // 自定义规则集的启用 / 禁用
     // 'stylistic/max-line-length': null,
